37592bcf265a8cd09bf906200c1baf5c574f4b69
Jack-Langloh <jack.langloh@tcu.edu>
2026-04-09 13:30:30 -0500

    Refactor Streamlit app code for clarity and structure
    Refactor code structure and improve readability by reorganizing imports and modifying function definitions.

--- Portfolio/StreamlitApp_HW5_Part_1.py
+++ Portfolio/StreamlitApp_HW5_Part_1.py
@@ -115,11 +115,10 @@ def display_explanation(input_df, session, aws_bucket):
 
     best_pipeline = load_pipeline(session, aws_bucket, 'sklearn-pipeline-deployment')
     
-    preprocessing_pipeline = Pipeline(steps=best_pipeline.steps[0:2]) 
-    input_df_transformed = preprocessing_pipeline.transform(input_df) 
-    feature_names = best_pipeline[0:2].get_feature_names_out() 
-    input_df_transformed = pd.DataFrame(input_df_transformed, columns=feature_names) 
-    shap_values = explainer(input_df_transformed) 
+    preprocessing_pipeline = Pipeline(steps=best_pipeline.steps[:-1])
+    input_df_transformed = preprocessing_pipeline.transform(input_df)
+    input_df_transformed = pd.DataFrame(input_df_transformed)
+    shap_values = explainer(input_df_transformed)
   
     st.subheader("🔍 Decision Transparency (SHAP)")
     fig, ax = plt.subplots(figsize=(10, 4))
