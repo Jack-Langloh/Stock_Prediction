b53aea7ee4fdb794c58fc364ff242ca884cfba16
Jack-Langloh <jack.langloh@tcu.edu>
2026-04-09 13:28:19 -0500

    Update feature_utils.py

--- src/feature_utils.py
+++ src/feature_utils.py
@@ -125,26 +125,25 @@ def convert_input_pca_regression(request_body, request_content_type):
 
         return_period = 5
 
-        SP500_1 = 'FDX_CR_Cum'
-        FDX_CR_Cum = json.loads(request_body)[SP500_1]
-        SP500_2 = 'UPS_CR_Cum'
-        UPS_CR_Cum = json.loads(request_body)[SP500_2]
+        SP500_1 = 'IBM_CR_Cum'
+        IBM_CR_Cum = json.loads(request_body)[SP500_1]
+        SP500_2 = 'NVDA_CR_Cum'
+        NVDA_CR_Cum = json.loads(request_body)[SP500_2]
 
-        X = np.log(dataset.drop([target],axis=1)).diff(return_period)
+        X = np.log(dataset.drop([target], axis=1)).diff(return_period)
         X = np.exp(X).cumsum()
         X.columns = [name + "_CR_Cum" for name in X.columns]
         
-        # Calculate the distance
         distances = np.sqrt(
-            (X[SP500_1] - FDX_CR_Cum)**2 + 
-            (X[SP500_2] - UPS_CR_Cum)**2
+            (X[SP500_1] - IBM_CR_Cum)**2 + 
+            (X[SP500_2] - NVDA_CR_Cum)**2
         )
         
         closest_index = distances.idxmin()
         closest_row = X.loc[[closest_index]]
     
-        closest_row[SP500_1] = FDX_CR_Cum
-        closest_row[SP500_2] = UPS_CR_Cum
+        closest_row[SP500_1] = IBM_CR_Cum
+        closest_row[SP500_2] = NVDA_CR_Cum
     
         return closest_row
     
